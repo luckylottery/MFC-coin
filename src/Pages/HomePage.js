990a3594107796dfa7b40a3032909bc2c7f228f1
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-10-05 19:30:01 -0500

    Added header

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,12 @@
+import { Header } from "../components/Header/Header"
+
 export const HomePage = () => {
   return (
-    <div>
-      HomePage      
+    <div className="md:w-5/6 mx-auto">
+      <Header />
+      <div className="mx-auto w-5/6	 bg-blue-500">
+
+      </div>
     </div>
   )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import { Image } from "../Image/Image"
+
+export const Header = () => {
+  return (
+    <header className="flex items-center px-5 py-2 gap-5">
+      <Link to="/home">
+        <Image
+          src="/assets/Logo Square BG White-01.png"
+          className="rounded-full h-16 w-16 lg:h-20 xl:w-24 xl:h-24"
+        />
+      </Link>
+      <Image
+        src="https://www.publicdomainpictures.net/pictures/150000/velka/banner-header-tapete-1450520058Odj.jpg"
+        className="w-full h-20 rounded"
+      />
+    </header>
+  )
+}
