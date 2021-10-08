a0febb2ebdea78207f40e98510a1e168666b1c93
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-10-08 12:27:29 -0500

    Added footer

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,6 +2,7 @@ import { Banner } from "../components/Banner/Banner"
 import { CardButtons } from "../components/Cards/CardButtons/CardButtons"
 import { CardImage } from "../components/Cards/CardImage/CardImage"
 import { Carousel } from "../components/Carousel/Carousel"
+import { Footer } from "../components/Footer/Footer"
 import { Header } from "../components/Header/Header"
 import { Image } from "../components/Image/Image"
 
@@ -11,7 +12,7 @@ export const HomePage = () => {
       <Header />
       <Carousel />
       <Banner />
-      <div className="flex flex-wrap justify-center gap-10 my-5 max-w-6xl mx-auto">
+      <div className="flex flex-wrap justify-center gap-10 my-16 max-w-6xl mx-auto">
         <CardButtons buttons={["Launch", "Mint", "Lock"]} />
         <CardImage image="http://www.gamengadgets.com/wp-content/uploads/2019/03/lottery.jpg" buttonText="PLAY NOW" />
         <CardButtons buttons={["Slots", "Poker", "Roulette", "Blackjack"]} />
@@ -19,6 +20,7 @@ export const HomePage = () => {
         <CardImage image="http://www.gamengadgets.com/wp-content/uploads/2019/03/lottery.jpg" buttonText="Factory Swap DEX" />
         <CardImage image="http://www.gamengadgets.com/wp-content/uploads/2019/03/lottery.jpg" />
       </div>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import { Image } from "../Image/Image"
+
+export const Footer = () => {
+  return (
+    <Image
+      src="https://lh3.googleusercontent.com/proxy/W_LEsW3slBukpSqk88-BeUppgm_QbinZQ7WfaE0AaI3ssXSFYL8EoG0nddqNxedg2d4q1ztmaUlPVVOzfFFgEV1INvfuP-0o9D8guVxI2szFtF5p0W_UGL09m6rnV9jF0g"
+      className="h-48"
+    />
+  )
+}
