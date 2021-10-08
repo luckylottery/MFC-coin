f986bb5ce99dbbd40c9f133946f93b59e9337beb
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-10-08 12:04:09 -0500

    Added cards section

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,9 @@
 import { Banner } from "../components/Banner/Banner"
+import { CardButtons } from "../components/Cards/CardButtons/CardButtons"
+import { CardImage } from "../components/Cards/CardImage/CardImage"
 import { Carousel } from "../components/Carousel/Carousel"
 import { Header } from "../components/Header/Header"
+import { Image } from "../components/Image/Image"
 
 export const HomePage = () => {
   return (
@@ -8,6 +11,14 @@ export const HomePage = () => {
       <Header />
       <Carousel />
       <Banner />
+      <div className="flex flex-wrap justify-center gap-10 my-5 max-w-6xl mx-auto">
+        <CardButtons buttons={["Launch", "Mint", "Lock"]} />
+        <CardImage image="http://www.gamengadgets.com/wp-content/uploads/2019/03/lottery.jpg" buttonText="PLAY NOW" />
+        <CardButtons buttons={["Slots", "Poker", "Roulette", "Blackjack"]} />
+        <CardImage image="http://www.gamengadgets.com/wp-content/uploads/2019/03/lottery.jpg" buttonText="Get Started" />
+        <CardImage image="http://www.gamengadgets.com/wp-content/uploads/2019/03/lottery.jpg" buttonText="Factory Swap DEX" />
+        <CardImage image="http://www.gamengadgets.com/wp-content/uploads/2019/03/lottery.jpg" />
+      </div>
     </>
   )
 }
