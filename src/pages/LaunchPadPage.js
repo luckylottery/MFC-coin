e9d37a09c5deabde81b94929c2d99c5be31c0452
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-10-08 14:21:56 -0500

    Initial launchpad screen configuration

--- src/pages/LaunchPadPage.js
+++ src/pages/LaunchPadPage.js
@@ -1,7 +1,11 @@
+import { Banner } from "../components/Banner/Banner"
+import { Header } from "../components/Header/Header"
+
 export const LaunchPadPage = () => {
   return (
-    <div>
-      LaunchpadPage
-    </div>
+    <>
+      <Header />
+      <Banner />
+    </>
   )
 }
