27db883859705e6d63d7a529c306d30e54dba8c1
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-10-09 16:26:25 -0500

    FInished launchpad page

--- src/pages/LaunchPadPage.js
+++ src/pages/LaunchPadPage.js
@@ -1,13 +1,17 @@
-import { Banner } from "../components/Banner/Banner"
 import { Drawer } from "../components/Drawer/Drawer"
 import { Header } from "../components/Header/Header"
+import { LaunchpadContent } from "../components/LaunchpadContent/LaunchpadContent"
+import { SearchUtility } from "../components/SearchUtility/SearchUtility"
 
 export const LaunchPadPage = () => {
   return (
     <>
       <Header />
-      <Banner />
       <Drawer />
+      <div className="flex gap-10 mx-auto mt-10 w-full md:w-11/12">
+        <SearchUtility />
+        <LaunchpadContent />
+      </div>
     </>
   )
 }
