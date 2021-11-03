689a964c294425cd048900a3a546766c5406915f
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-11-02 19:41:16 -0600

    FInished with carousel component

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,50 +1,36 @@
 import RMCarousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { Image } from "../Image/Image";
+
+const responsive = {
+  level1: {
+    breakpoint: { max: 4000, min: 0 },
+    items: 1
+  },
+};
 
 export const Carousel = () => {
-  const responsive = {    
-    level1: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    },
-    level2: {
-      breakpoint: { max: 768, min: 464 },
-      items: 2
-    },
-    level3: {
-      breakpoint: { max: 1024, min: 768 },
-      items: 3
-    },
-    level4: {
-      breakpoint: { max: 1200, min: 1024 },
-      items: 4
-    },
-    level5: {
-      breakpoint: { max: 4000, min: 1200 },
-      items: 5
-    },
-  };
-
   return (
-    <div className="w-full border-b-2 border-t-2 px-5">
-      <RMCarousel
-        responsive={responsive}
-        autoPlay={true}
-        arrows={false}
-        infinite={true}
-      >
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-        <div className="text-center">Memecoin: <span>10.51 USD</span></div>
-      </RMCarousel>
-    </div>
+    <RMCarousel
+      responsive={responsive}
+      autoPlay={true}
+      arrows={true}
+      infinite={true}
+      showDots={true}
+      className="mx-auto my-10 w-11/12 h-72 rounded"
+    >
+      <Image
+        src="https://i2.wp.com/criptotendencia.com/wp-content/uploads/2019/09/Gods-Unchained-Crypto-Game.jpg?fit=1200%2C630&ssl=1"
+        className="w-full h-72"
+      />
+      <Image
+        src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
+        className="w-full h-72"
+      />
+      <Image
+        src="https://www.crypto-news-flash.com/wp-content/uploads/2020/01/cover_cryptogames.jpg"
+        className="w-full h-72"
+      />
+    </RMCarousel>
   )
 }
