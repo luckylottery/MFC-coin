728d91d0d8c62d6ad9fb14eadf10785c60acba72
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-11-03 19:53:39 -0600

    Changed drawer styles

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { GrMenu } from "react-icons/gr";
+import { routes } from "../../routes";
 
 import { Image } from "../Image/Image";
 
@@ -14,12 +15,12 @@ export const Drawer = () => {
         onClick={() => setIsOpen(true)}
       />
       <div className={`${isOpen ? "w-screen h-screen" : "w-0 h-0"} fixed transition top-0 left-0 z-0 drawer-container z-10 lg:hidden`}>
-        <div className={`${isOpen ? "w-11/12 p-5" : "w-0"} fixed duration-300 top-0 left-0 h-full bg-white`}>
+        <div className={`${isOpen ? "w-11/12 sm:w-1/2 p-5 bg-red-300" : "w-0"} fixed duration-300 top-0 left-0 h-full bg-white`}>
           <div className={`${isOpen ? "flex" : "hidden"} flex-col justify-start gap-5 h-full`}>
             <div className="flex justify-between items-center">
               <Image
                 src="/assets/Logo Square BG White-01.png"
-                className="h-20 w-20"
+                className="h-20 w-20 rounded"
               />
               <button
                 className="text-xl bg-gray-300 rounded p-2 font-bold"
@@ -28,13 +29,38 @@ export const Drawer = () => {
                 CLOSE
               </button>
             </div>
-            <nav className="flex flex-col gap-5 text-2xl">
-              <label>KYC</label>
-              <label>Network</label>
-              <label>HC/SC</label>
-              <label>Starting Soon (Next 1 Hour)</label>
-              <label>Audit</label>
-              <label>Others</label>
+            <nav className="flex flex-col text-2xl">
+              {
+                routes.map(({ link, name, children, icon: Icon }) => (
+                  <div
+                    key={link}
+                    className={`${isOpen ? "py-3 hover:bg-blue-100" : "py-1"} flex flex-col px-5 transition-all	duration-700 cursor-pointer`}
+                  >
+                    <div className="flex gap-5 items-center">
+                      <Icon className="text-3xl" />
+                      <h1 className={`${isOpen ? "block" : "hidden"} text-blue-900 font-bold text-2xl`}>{name}</h1>
+                    </div>
+                    <div className={`${isOpen ? "block" : "hidden"}`}>
+                      {
+                        children && (
+                          <div className="ml-10">
+                            {
+                              children.map(({ link, name }) => (
+                                <h2
+                                  key={link}
+                                  className="rounded py-1 px-2 transition-all	duration-700 text-xl hover:bg-blue-700 hover:text-white"
+                                >
+                                  {name}
+                                </h2>
+                              ))
+                            }
+                          </div>
+                        )
+                      }
+                    </div>
+                  </div>
+                ))
+              }
             </nav>
           </div>
         </div>
