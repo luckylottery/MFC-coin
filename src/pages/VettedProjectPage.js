fbcf70491ba16d87346b1708a26de44ebd00ea48
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-11-17 12:27:26 -0600

    Completed vetter project screen

--- src/pages/VettedProjectPage.js
+++ src/pages/VettedProjectPage.js
@@ -37,6 +37,7 @@ export const VettedProjectPage = () => {
               <div className="flex gap-5 mb-5 sm:mb-0 justify-center items-center">
                 <img
                   className="h-20 max-w-full"
+                  alt="logo"
                   src={vettedProjectData.image}
                 />
                 <h1 className="text-xl font-bold text-center">{vettedProjectData.name}</h1>
@@ -121,6 +122,84 @@ export const VettedProjectPage = () => {
                       />
                     </div>
                   </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col md:flex-row gap-10 text-gray-700 font-bold">
+              <div className="flex-1">
+                <h1 className="mb-2.5 text-xl">PROJECT INFORMATION</h1>
+                <div className="border-t-2 border-b p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Name</h2>
+                  <p className="flex-1 text-gray-900">{vettedProjectData.name}</p>
+                </div>
+                <div className="border-t border-b p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Chain</h2>
+                  <p className="flex-1 uppercase text-gray-900">{vettedProjectData.chain}</p>
+                </div>
+                <div className="border-t border-b p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Website</h2>
+                  <a
+                    className="flex-1 text-gray-900 cursor-pointer break-all"
+                    href={vettedProjectData.website}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {vettedProjectData.website}
+                  </a>
+                </div>
+                <div className="border-t border-b p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Socials</h2>
+                  <div className="flex-1 flex gap-1.5 text-gray-900">
+                    {
+                      vettedProjectData.socials.map(({ icon: Icon, link }, index) => (
+                        <a
+                          href={link}
+                          target="_blank" rel="noreferrer"
+                        >
+                          <Icon key={index}
+                            className="h-5 w-5"
+                          />
+                        </a>
+                      ))
+                    }
+                  </div>
+                </div>
+                <div className="border-t border-b p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">White Paper</h2>
+                  <a
+                    className="flex-1 text-gray-900"
+                    href={vettedProjectData.whitePaper}
+                    target="_blank" rel="noreferrer"
+                  >
+                    Open White Paper
+                  </a>
+                </div>
+                <div className="border-t border-b-2 p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Security Audit Report</h2>
+                  <p className="flex-1 text-gray-900">{vettedProjectData.securityAuditReport}</p>
+                </div>
+              </div>
+              <div className="flex-1">
+                <h1 className="mb-2.5 text-xl">TOKEN INFORMATION</h1>
+                <div className="border-t-2 border-b p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Name</h2>
+                  <p className="flex-1 text-gray-900">{vettedProjectData.tokenInformation.name}</p>
+                </div>
+                <div className="border-t border-b p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Symbol</h2>
+                  <p className="flex-1 text-gray-900">{vettedProjectData.tokenInformation.symbol}</p>
+                </div>
+                <div className="border-t border-b p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Decimals</h2>
+                  <p className="flex-1 text-gray-900">{vettedProjectData.tokenInformation.decimals}</p>
+                </div>
+                <div className="border-t border-b p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Address</h2>
+                  <p className="flex-1 text-gray-900">{vettedProjectData.tokenInformation.address}</p>
+                </div>
+                <div className="border-t border-b-2 p-2.5 border-gray-500 flex gap-2.5">
+                  <h2 className="flex-1">Total Supply</h2>
+                  <p className="flex-1 text-gray-900">{vettedProjectData.tokenInformation.totalSupply}</p>
                 </div>
               </div>
             </div>
