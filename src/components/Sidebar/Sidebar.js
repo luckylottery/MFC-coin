8770d6fd0d53d0c813f7cc824c3e8e40b31f4986
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-11-23 19:21:59 -0600

    Added menu icon to sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,6 +4,8 @@ import { GrMenu } from "react-icons/gr";
 import { Image } from "../Image/Image";
 import { routes } from "../../routes";
 
+const burgerMenuIcon = require('../../assets/Icons/Burger Menu.png').default;
+
 export const Sidebar = () => {
 
   const [isOpen, setIsOpen] = useState(true);
@@ -29,12 +31,17 @@ export const Sidebar = () => {
         className={`hidden lg:flex flex-col items-center relative bg-blue-4 h-screen ${isOpen ? "lg:w-3/12 xl:w-2/12" : "w-16"}`} />
       <div
         className={`hidden lg:flex lg:fixed flex-col items-center relative bg-blue-4 shadow-lg h-screen transition-all z-50 ${isOpen ? "lg:w-3/12 xl:w-2/12" : "w-16"}`}>
-        {/* <div
-          className={`${isOpen ? "block" : "hidden"} absolute -right-5 top-10 bg-blue-1 p-2.5 rounded cursor-pointer`}
+        <div
+          className={`${isOpen ? "block" : "hidden"} absolute -right-11 top-10 p-2.5 rounded cursor-pointer`}
           onClick={toggleState}
         >
-          <GrMenu className="text-2xl font-bold" />
-        </div> */}
+          <div
+            className="h-14 w-14 bg-no-repeat bg-contain bg-center"
+            style={{
+              backgroundImage: `url("${burgerMenuIcon}")`
+            }}
+          />
+        </div>
         <div className="pt-5 h-full w-full flex justify-center w-full flex-col gap-10">
           {
             isOpen ? (
@@ -43,8 +50,11 @@ export const Sidebar = () => {
                 className="self-center rounded-full h-16 w-16 lg:h-20 xl:w-24 xl:h-24"
               />
             ) : (
-              <GrMenu
-                className="font-bold block text-2xl cursor-pointer mx-auto"
+              <div
+                className="cursor-pointer mx-auto h-10 w-10 bg-no-repeat bg-contain bg-center"
+                style={{
+                  backgroundImage: `url("${burgerMenuIcon}")`
+                }}
                 onClick={toggleState}
               />
             )
@@ -62,7 +72,7 @@ export const Sidebar = () => {
                         to={link}
                         className="flex gap-5 items-center"
                       >
-                        <div 
+                        <div
                           className="h-5 w-5 bg-no-repeat bg-center bg-contain"
                           style={{
                             backgroundImage: `url("${icon}")`
@@ -75,7 +85,7 @@ export const Sidebar = () => {
                         className="flex gap-5 items-center"
                         onClick={() => toggleCollapsableItems(name)}
                       >
-                        <div 
+                        <div
                           className="h-5 w-5 bg-no-repeat bg-center bg-contain"
                           style={{
                             backgroundImage: `url("${icon}")`
