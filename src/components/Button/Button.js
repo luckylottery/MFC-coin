dfc6508ddbc4e6bc5e34f0f30f2f518214b82e92
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-11-23 18:00:55 -0600

    Added button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,8 @@
 export const Button = ({ color, text }) => {
   return (
     <button className={`${color === "red" ?
-        "border-red-600 bg-red-100 text-red-600 hover:bg-red-600" :
-        "border-blue-600 bg-blue-100 text-blue-600 hover:bg-blue-600"
+        "border-red bg-pink text-red hover:bg-red" :
+        "border-blue-1 text-blue-1 hover:bg-blue-1"
       } border border-b-4 rounded-xl px-5 py-2.5 font-bold mx-auto transition duration-300 hover:text-white`}
     >
       {text}
