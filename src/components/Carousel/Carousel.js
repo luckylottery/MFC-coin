439b29e7d4d5708254c65a3310c7dcb782870a16
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-11-26 19:52:24 -0600

    Added css trick to show cards content on carrousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -21,51 +21,98 @@ const responsive = {
   }
 };
 
+
 export const Carousel = () => {
   return (
-    <RMCarousel
-      responsive={responsive}
-      autoPlay={true}
-      arrows={true}
-      infinite={true}
-      className="mx-auto my-10 w-full md:w-11/12 text-center bg-red h-72 rounded z-0"
-    >
-      <Image
-        src="https://i2.wp.com/criptotendencia.com/wp-content/uploads/2019/09/Gods-Unchained-Crypto-Game.jpg?fit=1200%2C630&ssl=1"
-        className="w-11/12 h-72 mx-auto"
-      />
-      <Image
-        src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
-        className="w-11/12 h-72 mx-auto"
-      />
-      <Image
-        src="https://www.crypto-news-flash.com/wp-content/uploads/2020/01/cover_cryptogames.jpg"
-        className="w-11/12 h-72 mx-auto"
-      />
-      <Image
-        src="https://i2.wp.com/criptotendencia.com/wp-content/uploads/2019/09/Gods-Unchained-Crypto-Game.jpg?fit=1200%2C630&ssl=1"
-        className="w-11/12 h-72 mx-auto"
-      />
-      <Image
-        src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
-        className="w-11/12 h-72 mx-auto"
-      />
-      <Image
-        src="https://www.crypto-news-flash.com/wp-content/uploads/2020/01/cover_cryptogames.jpg"
-        className="w-11/12 h-72 mx-auto"
-      />
-      <Image
-        src="https://i2.wp.com/criptotendencia.com/wp-content/uploads/2019/09/Gods-Unchained-Crypto-Game.jpg?fit=1200%2C630&ssl=1"
-        className="w-11/12 h-72 mx-auto"
-      />
-      <Image
-        src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
-        className="w-11/12 h-72 mx-auto"
-      />
-      <Image
-        src="https://www.crypto-news-flash.com/wp-content/uploads/2020/01/cover_cryptogames.jpg"
-        className="w-11/12 h-72 mx-auto"
-      />
-    </RMCarousel>
+    <div className="overflow-hidden mx-auto mt-10 mb-40 h-72 w-full md:w-11/12 text-center">
+      <RMCarousel
+        responsive={responsive}
+        // autoPlay={true}
+        arrows={true}
+        infinite={true}
+        className="rounded z-0 overflow-visible"
+      >
+        <div className="relative">
+          <Image
+            src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
+            className="w-11/12 h-40 mx-auto rounded-3xl"
+          />
+          <div className="absolute -bottom-24 text-left px-5 h-20 text-blue-1 w-full">
+            <h1 className="font-bold">CoinMarketCap</h1>
+            <p className="text-sm"> tempor incididunt ut labore et dolore magna aliqua. </p>
+          </div>
+        </div>
+        <div className="relative">
+          <Image
+            src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
+            className="w-11/12 h-40 mx-auto rounded-3xl"
+          />
+          <div className="absolute -bottom-24 text-left px-5 h-20 text-blue-1 w-full">
+            <h1 className="font-bold">CoinMarketCap</h1>
+            <p className="text-sm"> tempor incididunt ut labore et dolore magna aliqua. </p>
+          </div>
+        </div>
+        <div className="relative">
+          <Image
+            src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
+            className="w-11/12 h-40 mx-auto rounded-3xl"
+          />
+          <div className="absolute -bottom-24 text-left px-5 h-20 text-blue-1 w-full">
+            <h1 className="font-bold">CoinMarketCap</h1>
+            <p className="text-sm"> tempor incididunt ut labore et dolore magna aliqua. </p>
+          </div>
+        </div>
+        <div className="relative">
+          <Image
+            src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
+            className="w-11/12 h-40 mx-auto rounded-3xl"
+          />
+          <div className="absolute -bottom-24 text-left px-5 h-20 text-blue-1 w-full">
+            <h1 className="font-bold">CoinMarketCap</h1>
+            <p className="text-sm"> tempor incididunt ut labore et dolore magna aliqua. </p>
+          </div>
+        </div>
+        <div className="relative">
+          <Image
+            src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
+            className="w-11/12 h-40 mx-auto rounded-3xl"
+          />
+          <div className="absolute -bottom-24 text-left px-5 h-20 text-blue-1 w-full">
+            <h1 className="font-bold">CoinMarketCap</h1>
+            <p className="text-sm"> tempor incididunt ut labore et dolore magna aliqua. </p>
+          </div>
+        </div>
+        <div className="relative">
+          <Image
+            src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
+            className="w-11/12 h-40 mx-auto rounded-3xl"
+          />
+          <div className="absolute -bottom-24 text-left px-5 h-20 text-blue-1 w-full">
+            <h1 className="font-bold">CoinMarketCap</h1>
+            <p className="text-sm"> tempor incididunt ut labore et dolore magna aliqua. </p>
+          </div>
+        </div>
+        <div className="relative">
+          <Image
+            src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
+            className="w-11/12 h-40 mx-auto rounded-3xl"
+          />
+          <div className="absolute -bottom-24 text-left px-5 h-20 text-blue-1 w-full">
+            <h1 className="font-bold">CoinMarketCap</h1>
+            <p className="text-sm"> tempor incididunt ut labore et dolore magna aliqua. </p>
+          </div>
+        </div>
+        <div className="relative">
+          <Image
+            src="https://www.esports.net/wp-content/uploads/2021/08/Crypto-Pop.jpg"
+            className="w-11/12 h-40 mx-auto rounded-3xl"
+          />
+          <div className="absolute -bottom-24 text-left px-5 h-20 text-blue-1 w-full">
+            <h1 className="font-bold">CoinMarketCap</h1>
+            <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          </div>
+        </div>
+      </RMCarousel>
+    </div>
   )
 }
