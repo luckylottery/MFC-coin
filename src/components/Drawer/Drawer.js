8605172ef8d4ff561b71924ada65a3119e762ef4
Br4ndonP0nce <brandoncompany876@gmail.com>
2021-12-14 02:28:10 +0100

    Added hotfixes

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,13 +1,12 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { routes } from "../../routes";
 
-const burgerMenuIcon = require('../../assets/Icons/Burger Menu.png').default;
-const MCFIcon = require('../../assets/Icons/LOGO MF_ICON.png').default;
-const MCFFrontIcon = require('../../assets/Icons/coin frontface.png').default;
+const burgerMenuIcon = require("../../assets/Icons/Burger Menu.png").default;
+const MCFIcon = require("../../assets/Icons/LOGO MF_ICON.png").default;
+const MCFFrontIcon = require("../../assets/Icons/coin frontface.png").default;
 
 export const Drawer = () => {
-
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -15,13 +14,25 @@ export const Drawer = () => {
       <div
         className="h-14 w-14 bg-no-repeat bg-contain bg-center lg:hidden"
         style={{
-          backgroundImage: `url("${burgerMenuIcon}")`
+          backgroundImage: `url("${burgerMenuIcon}")`,
         }}
         onClick={() => setIsOpen(true)}
       />
-      <div className={`${isOpen ? "w-screen h-screen" : "w-0 h-0"} fixed transition top-0 left-0 z-0 drawer-container z-10 lg:hidden`}>
-        <div className={`${isOpen ? "w-11/12 sm:w-1/2 p-5 bg-red-300" : "w-0"} fixed text-blue-1 duration-300 top-0 left-0 h-full bg-white`}>
-          <div className={`${isOpen ? "flex" : "hidden"} flex-col justify-start gap-5 h-full`}>
+      <div
+        className={`${
+          isOpen ? "w-screen h-screen" : "w-0 h-0"
+        } fixed transition top-0 left-0 z-0 drawer-container z-10 lg:hidden`}
+      >
+        <div
+          className={`${
+            isOpen ? "w-11/12 sm:w-1/2 p-5 bg-red-300" : "w-0"
+          } fixed text-blue-1 duration-300 top-0 left-0 h-full bg-white`}
+        >
+          <div
+            className={`${
+              isOpen ? "flex" : "hidden"
+            } flex-col justify-start gap-5 h-full`}
+          >
             <div className="flex justify-between items-center">
               <div
                 className="h-20 w-20 bg-no-repeat bg-contain bg-center"
@@ -35,59 +46,54 @@ export const Drawer = () => {
               </button>
             </div>
             <nav className="flex flex-col text-2xl">
-              {
-                routes.map(({ link, name, children, icon, collapsable }) => (
-                  <div
-                    key={`${link}_drawer`}
-                    className={`${isOpen ? "py-3" : "py-1"} flex flex-col px-5 transition-all	duration-700 cursor-pointer`}
-                  >
-                    {
-                      !collapsable ? (
-                        <Link
-                          className="flex gap-5 items-center"
-                          to={link}
-                        >
-                          <div
-                            className="h-5 w-5 bg-no-repeat bg-center bg-contain"
-                            style={{
-                              backgroundImage: `url("${icon}")`
-                            }}
-                          />
-                          <h1 className={`${isOpen ? "block" : "hidden"} text-2xl`}>{name}</h1>
-                        </Link>
-                      ) : (
-                        <div className="flex gap-5 items-center">
-                          <div
-                            className="h-5 w-5 bg-no-repeat bg-center bg-contain"
-                            style={{
-                              backgroundImage: `url("${icon}")`
-                            }}
-                          />
-                          <h1 className={`${isOpen ? "block" : "hidden"} text-2xl`}>{name}</h1>
-                        </div>
-                      )
-                    }
-                    <div className={`${isOpen ? "block" : "hidden"}`}>
-                      {
-                        children && (
-                          <div className="ml-10">
-                            {
-                              children.map(({ link, name }) => (
-                                <h2
-                                  key={link}
-                                  className="rounded py-1 px-2 transition-all	duration-700 text-xl"
-                                >
-                                  {name}
-                                </h2>
-                              ))
-                            }
-                          </div>
-                        )
-                      }
+              {routes.map(({ link, name, children, icon, collapsable }) => (
+                <div
+                  key={`${link ? link : name}_drawer`}
+                  className={`${
+                    isOpen ? "py-3" : "py-1"
+                  } flex flex-col px-5 transition-all	duration-700 cursor-pointer`}
+                >
+                  {!collapsable ? (
+                    <Link className="flex gap-5 items-center" to={link}>
+                      <div
+                        className="h-5 w-5 bg-no-repeat bg-center bg-contain"
+                        style={{
+                          backgroundImage: `url("${icon}")`,
+                        }}
+                      />
+                      <h1 className={`${isOpen ? "block" : "hidden"} text-2xl`}>
+                        {name}
+                      </h1>
+                    </Link>
+                  ) : (
+                    <div className="flex gap-5 items-center">
+                      <div
+                        className="h-5 w-5 bg-no-repeat bg-center bg-contain"
+                        style={{
+                          backgroundImage: `url("${icon}")`,
+                        }}
+                      />
+                      <h1 className={`${isOpen ? "block" : "hidden"} text-2xl`}>
+                        {name}
+                      </h1>
                     </div>
+                  )}
+                  <div className={`${isOpen ? "block" : "hidden"}`}>
+                    {children && (
+                      <div className="ml-10">
+                        {children.map(({ link, name }) => (
+                          <h2
+                            key={link}
+                            className="rounded py-1 px-2 transition-all	duration-700 text-xl"
+                          >
+                            {name}
+                          </h2>
+                        ))}
+                      </div>
+                    )}
                   </div>
-                ))
-              }
+                </div>
+              ))}
             </nav>
             <div className="px-5 my-10 text-blue-1 font-bold">
               <div className="bg-blue-1 w-full h-1 rounded mb-5" />
@@ -95,7 +101,7 @@ export const Drawer = () => {
                 <div
                   className="h-9 w-9 bg-no-repeat bg-center bg-contain"
                   style={{
-                    backgroundImage: `url("${MCFFrontIcon}")`
+                    backgroundImage: `url("${MCFFrontIcon}")`,
                   }}
                 />
                 <label>$0,00034</label>
@@ -109,5 +115,5 @@ export const Drawer = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
